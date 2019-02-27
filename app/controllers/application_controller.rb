081b5dcd7d33b44f0abab428358039f001b80b2b
Linda Chang <lindachang186@gmail.com>
2019-02-27 16:54:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
     if @user && @user.password == params[:password]
       session[:user_id]=@user.id
       redirect '/account'
-    else
+    end 
       redirect '/error'
     end
   end
